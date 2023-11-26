57ac566e07cd1e13443de074a219fb0934a9d0a0
Xytra-zn <143930236+Xytra-zn@users.noreply.github.com>
2023-11-27 02:13:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from telegram import Update, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 from shayari import shayari_list
+from jokes import jokes_list
 import random
 import os
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Welcome! Use the /sspam command to get Shayari. Type /cancel to stop receiving Shayari.")
+    update.message.reply_text("Welcome! Use the /sspam command for Shayari and /joke command for jokes. Type /cancel to stop receiving messages.")
 
 def sspam(update: Update, context: CallbackContext) -> None:
     args = context.args
@@ -14,26 +15,47 @@ def sspam(update: Update, context: CallbackContext) -> None:
         return
 
     try:
-        num_shayari = int(args[0])
+        num_messages = int(args[0])
     except ValueError:
         update.message.reply_text("Please enter a valid number.")
         return
 
-    if num_shayari <= 0:
+    if num_messages <= 0:
         update.message.reply_text("Please enter a positive number.")
         return
 
-    total_shayari = len(shayari_list)
-    if num_shayari >= total_shayari:
-        selected_shayari = shayari_list * (num_shayari // total_shayari) + random.sample(shayari_list, num_shayari % total_shayari)
+    total_messages = len(shayari_list)
+    if num_messages >= total_messages:
+        selected_messages = shayari_list * (num_messages // total_messages) + random.sample(shayari_list, num_messages % total_messages)
     else:
-        selected_shayari = random.sample(shayari_list, num_shayari)
+        selected_messages = random.sample(shayari_list, num_messages)
 
-    for shayari in selected_shayari:
-        update.message.reply_text(shayari)
+    for message in selected_messages:
+        update.message.reply_text(message)
+
+def joke(update: Update, context: CallbackContext) -> None:
+    args = context.args
+    if not args:
+        update.message.reply_text("Please use the command in the format `/joke`.")
+        return
+
+    num_jokes = 1
+    try:
+        num_jokes = int(args[0])
+    except ValueError:
+        update.message.reply_text("Please enter a valid number.")
+
+    total_jokes = len(jokes_list)
+    if num_jokes >= total_jokes:
+        selected_jokes = jokes_list * (num_jokes // total_jokes) + random.sample(jokes_list, num_jokes % total_jokes)
+    else:
+        selected_jokes = random.sample(jokes_list, num_jokes)
+
+    for joke in selected_jokes:
+        update.message.reply_text(joke)
 
 def cancel(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Shayari delivery canceled. Type /sspam to get more Shayari.")
+    update.message.reply_text("Message delivery canceled. Type /sspam for Shayari and /joke for jokes.")
 
 def alive(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("BOT IS ALIVE AND WORKING LIKE LION 🦁. AAKHIR BETA BHI TOH XYTRA KA HU AUR AMAN KA BHAI🙂", parse_mode=ParseMode.MARKDOWN)
@@ -44,6 +66,8 @@ def main() -> None:
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("sspam", sspam, pass_args=True))
+    dp.add_handler(CommandHandler("joke", joke, pass_args=True))
+    dp.add_handler(CommandHandler("jokes", joke, pass_args=True))  # Alias for /joke
     dp.add_handler(CommandHandler("cancel", cancel))
     dp.add_handler(CommandHandler("alive", alive))
 
